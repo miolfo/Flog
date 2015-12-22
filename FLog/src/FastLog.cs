6c042e37a89e5a6a1fc747bf04a03f502c4514a3
miolfo <miolfo@utu.fi>
2015-12-22 19:48:47 +0200

    Added support for timed entry adding

--- FLog/src/FastLog.cs
+++ FLog/src/FastLog.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
 
 namespace FLog
 {
@@ -17,6 +19,11 @@ namespace FLog
         private string _filePath;
         private string _fileName;
         private LogType _logType;
+
+        //Variables for using timed logging
+        TEntry _timerCallback;
+        private Timer _timer;
+        private bool _usingTimedLogging = false;
 
         public int MaxLogEntries
         {
@@ -56,6 +63,8 @@ namespace FLog
         /// <param name="fileName">name of the logfile</param>
         public FastLog(string path, string fileName, LogType type)
         {
+            //If log type is write and the log already exists, throw exception
+            if (File.Exists(path + "/" + fileName) && type == LogType.LOGTYPE_WRITE) throw new Exception("Can't write to already existing log file!"); 
             _logAccess = new FileAccesser(path, fileName);
             _filePath = path;
             _fileName = fileName;
@@ -87,7 +96,41 @@ namespace FLog
             }
         }
 
-        public IEnumerable<T> GetEntries()
+        public delegate T TEntry();
+
+        /// <summary>
+        /// A function that can be used to set the logger to write a new entry at defined intervals
+        /// </summary>
+        /// <param name="ms">Time between each consecutive log entry in ms</param>
+        /// <param name="entryCallback">A callback function that returns an entry, which is added to the log every ms milliseconds</param>
+        public void TimedEntry(int ms, TEntry entryCallback)
+        {
+            if (_usingTimedLogging)
+            {
+                Console.WriteLine("Timed logging already set! Stop logger before restarting");
+                return;
+            }
+            else
+            {
+                _usingTimedLogging = true;
+            }
+            _timerCallback = entryCallback;
+            _timer = new Timer(ms);
+            _timer.Elapsed += TimedLog;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Stop the timed logging
+        /// </summary>
+        public void StopTimedLogging()
+        {
+            _timer.Enabled = false;
+            _usingTimedLogging = false;
+        }
+
+        public List<T> GetEntries()
         {
             return _entries;
         }
@@ -103,6 +146,13 @@ namespace FLog
         #endregion
 
         #region private methods
+
+        private void TimedLog(Object source, ElapsedEventArgs e)
+        {
+            Log(_timerCallback());
+            Console.WriteLine("Added entry");
+        }
+
         private static string CreateLogfileName()
         {
             string name = "";
--- FLog/tests/Program.cs
+++ FLog/tests/Program.cs
@@ -8,24 +8,30 @@ namespace FLog
 {
     class Program
     {
+        public delegate SimpleEntry testCallback();
+        public static SimpleEntry asd()
+        {
+            SimpleEntry e = new SimpleEntry(0, "seppo hovi");
+            return e;            
+        }
+
         static void Main(string[] args)
         {
-            //FileAccesser writer = new FileAccesser("juu/", "asd.txt");
-            //writer.AppendFile("joopati joo");
-            /*String[] content = writer.GetFileContents();
-            foreach (String s in content)
-            {
-                Console.WriteLine(s);
-            }*/
-
-            /*FastLog logger = new FastLog("logTests", "testlog.flog", LogType.LOGTYPE_WRITE);
+            /*FastLog<SimpleEntry> logger = new FastLog<SimpleEntry>("logTests", "testlog.flog", LogType.LOGTYPE_WRITE);
             for (int i = 0; i < 123; i++)
             {
                 SimpleEntry e2 = new SimpleEntry(i, "joo");
                 logger.Log(e2);                
             }*/
-            FastLog<SimpleEntry> logger2 = new FastLog<SimpleEntry>("logTests", "testlog.flog", LogType.LOGTYPE_READ);
-            Console.WriteLine(logger2.GetEntryWithId(32).content);
+            /*FastLog<SimpleEntry> logger2 = new FastLog<SimpleEntry>("logTests", "testlog.flog", LogType.LOGTYPE_READ);
+            Console.WriteLine(logger2.GetEntryWithId(32).content);*/
+            FastLog<SimpleEntry> timedLogger = new FastLog<SimpleEntry>("logTests", "timedlog.flog", LogType.LOGTYPE_WRITE);
+            FastLog<SimpleEntry>.TEntry cb = asd;
+            timedLogger.TimedEntry(1000, cb);
+
+            while (timedLogger.GetEntries().Count < 5) {
+
+            }
         }
     }
 }
